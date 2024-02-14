f6692297af2a871e306fc67c38777eead924c0a9
Simon Lalonde <simlalonde@hotmail.com>
2024-02-14 10:51:45 -0500

    Implement copy constructor for deque

--- tp2/dequeImpl.h
+++ tp2/dequeImpl.h
@@ -23,6 +23,24 @@
 template <typename T>
 deque<T>::deque(const deque& source)
 {
+    m_size = source.size();    // Conserve la meme taille
+    if(m_size == 0)
+	{
+        m_cap = 0;
+        m_debut = nullptr;
+    }
+    else
+	{
+        m_cap = m_size + 1;
+        m_debut = new T*[m_cap];    // Allocation array de pointeurs incluant cap
+        for(size_t i = 0; i < m_size; ++i) 
+        {
+            m_debut[i] = new T(source[i]);    // Copie valeur dans nouvel espace pointe par m_debut[i]
+        }
+		m_debut[m_size] = nullptr;    // element apres dernier
+    }
+
+    m_zero = 0;    // Reset debut a premier pos
 }
 
 template <typename T>
