f114d61e617c86cf17e4d60ccca64eac1028cbbc
Simon Lalonde <simlalonde@hotmail.com>
2024-04-05 07:31:29 -0400

    Fix sizing on insert and refactor var names

--- tp6/unordered_multisetImpl.h
+++ tp6/unordered_multisetImpl.h
@@ -37,34 +37,35 @@ unordered_multiset<TYPE, classe_de_dispersion>::insert(const TYPE& val)
 {
     // Faire le hachage de la nouvelle val et localiser l'alveole
     size_t valHachage = disperseur(val);
-    size_t alv = valHachage % m_rep.size();
+    size_t nbAlv = m_rep.size() - 1;
+    size_t indexAlv = valHachage % nbAlv;
     
     // Creer une nouvelle alveole au besoin en fct du hachage
-    if (m_rep[alv] == nullptr)
+    if (m_rep[indexAlv] == nullptr)
     {
         std::list<TYPE>* pList = new std::list<TYPE>();
         pList->push_back(val);
-        m_rep[alv] = pList;
+        m_rep[indexAlv] = pList;
     }
     else
     {
-        m_rep[alv]->push_back(val);     // 
+        m_rep[indexAlv]->push_back(val);     // 
     }
 
     // Mise a jour de la taille
     m_size++;
 
     // Augmentation nb alveoles en fct du facteur de charge
-    float facteurCharge = static_cast<float>(m_size) / m_rep.size();       // moy nb ele par alveole
+    float facteurCharge = static_cast<float>(m_size) / nbAlv;
     if (facteurCharge > m_facteur_max)
     {
-        m_rep.resize(m_rep.size() * 2 + 1);
+        m_rep.resize(m_rep.size() * 2);
     }
-    // Iterator sur alveole d'insertion
-    typename std::vector<std::list<TYPE>*>::iterator itAlv = m_rep.begin() + alv;
+    // Iterator sur alveole d'insertion (random access donc on peut faire + alv)
+    typename std::vector<std::list<TYPE>*>::iterator itAlv = m_rep.begin() + indexAlv;
     
     // Iterator sur l'element insere dans la liste 
-    typename list<TYPE>::iterator itPos = m_rep[alv]->end();
+    typename list<TYPE>::iterator itPos = m_rep[indexAlv]->end();
     itPos--;       // Chercher le dernier ele
     
     // Retour de l'iterator ums
